872c0d1bdeac1fe19c3923037d135a45c88d7dc8
QuartzAl <55610038+QuartzAl@users.noreply.github.com>
2022-02-17 15:46:23 +0700

    remove firebase dependencies

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pickle
 import requests
-from firebase import firebase
 import socket
 from bs4 import BeautifulSoup
 import validators
@@ -8,36 +7,6 @@ from flask import Flask, render_template, request
 
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
-
-firebase = firebase.FirebaseApplication(
-    "https://mriksohoax-3fb0c-default-rtdb.asia-southeast1.firebasedatabase.app/", None)
-
-
-def checkUser():
-    users = firebase.get('/overviews/users/devices', None)
-    total = firebase.get('/overviews/users/total', None)
-    valid = True
-    for user in users:
-        if users[user]['ip'] == ip_address:
-            valid = False
-    if valid == True:
-        firebase.post('/overviews/users/devices', {"ip": ip_address})
-        firebase.put('/overviews/users', 'total', total+1)
-
-
-def checkNewsContent(content, result):
-    news = firebase.get('/overviews/news_detections/news', None)
-    total = firebase.get('/overviews/news_detections/total', None)
-    valid = True
-    for item in news:
-        if news[item]['content'] == content:
-            valid = False
-    if valid == True:
-        firebase.post('/overviews/news_detections/news', {
-            "content": content,
-            "status": result
-        })
-        firebase.put('/overviews/news_detections', 'total', total+1)
 
 
 def loadModel():
@@ -73,14 +42,11 @@ na = newsAnalyzer()
 
 @app.route('/', methods=['POST', 'GET'])
 def homepage():
-    overviews = firebase.get('/overviews', None)
-    return render_template("index.html", overviews=overviews)
+    return render_template("index.html")
 
 
 @app.route('/scan', methods=['POST', 'GET'])
 def upload():
-
-    overviews = firebase.get('/overviews', None)
 
     print("Form Submitted")
     text = ''
@@ -93,16 +59,11 @@ def upload():
             result = na.checkNews(text)
             text = text
 
-            checkUser()
-            checkNewsContent(text, result)
-
-            overviews = firebase.get('/overviews', None)
-
         if request.form['link'] != "":
 
             url = request.form['link']
             valid = validators.url(url)
-            if valid == True:
+            if valid:
                 res = requests.get(url)
                 html_page = res.content
                 soup = BeautifulSoup(html_page, 'html.parser')
@@ -129,19 +90,14 @@ def upload():
                 result = na.checkNews(output)
                 text = url
 
-                checkUser()
-                checkNewsContent(text, result)
-
-                overviews = firebase.get('/overviews', None)
             else:
                 result = 'Link not found'
 
-        return render_template("index.html", result=result, text=text, overviews=overviews)
+        return render_template("index.html", result=result, text=text)
 
     if request.method == "GET":
-        return render_template("index.html", overviews=overviews)
+        return render_template("index.html")
 
 
 if __name__ == '__main__':
-
     app.run()
